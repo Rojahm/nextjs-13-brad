efe1d1c97023020be6c7d040c151d651640d1888
Rojahm <100220824+Rojahm@users.noreply.github.com>
2023-08-03 22:34:43 +0330

    repo page that fetches and lists all the repositories

--- app/code/repos/page.jsx
+++ app/code/repos/page.jsx
@@ -2,7 +2,9 @@ import Link from "next/link";
 import { FaStar, FaCodeBranch, FaEye } from "react-icons/fa";
 
 async function fechRepos() {
-  const response = await fetch("https://api.github.com/users/Rojahm/repos");
+  const response = await fetch("https://api.github.com/users/Rojahm/repos", {
+    next: { revalidate: 60 },
+  });
   //wait 3 sec to load so loader can be seen!
   // await new Promise((resolve) => setTimeout(resolve, 3000));
 
